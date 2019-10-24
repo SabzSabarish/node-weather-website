3ece70a4c588bf1deb101f2b39fbfb04fe35e227
SabzSabarish <56868256+SabzSabarish@users.noreply.github.com>
2019-10-24 14:37:31 +0530

    Add about text

--- src/app.js
+++ src/app.js
@@ -29,14 +29,14 @@ app.get('', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.render('about', {
-        title: 'About Me',
+        title: 'About Us',
         name: 'Sabarish'
     })
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        helpText: 'This is some helpful text.',
+        helpText: 'Click here for more Details',
         title: 'Help',
         name: 'Sabarish'
     })
